ab69857e783f884bc60bc08621a977bc2b8969ea
KimmyTsai <kimmy567567@gmail.com>
2024-05-24 22:30:09 +0800

    Example

---
+++ basicLayout.java
@@ -0,0 +1,26 @@
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class basicLayout extends JFrame{
+	private JLabel txt;
+	public basicLayout(){
+		super("基本視窗範例");
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		c.setBackground(Color.white);
+		txt = new JLabel("文字範例");
+		c.add(txt);
+	}
+	public static void main(String[] args) {
+		basicLayout app = new basicLayout();
+		app.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		app.setSize(1920, 1080);
+		app.setVisible(true);
+	}
+
+}
